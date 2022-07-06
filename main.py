0069dfc169d89f1984fb523cd7dfa157e147704d
Kemal Yesildagli <KemalYesildagli@medatechuk.com>
2022-07-06 16:56:37 +0100

    Capitalize brand name

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def handle_files():
 def reorder_columns(path):
     df = pd.read_csv(path, delimiter=';')
     df["e"] = ''
+    df['Seller Brand Name'] = df['Seller Brand Name'].str.upper()
     df_reorder = df[['Seller MFPN','PDI  Item Product Description','e','e','e','e','e','e','e','e','e','e','e','Seller Brand Name','e','e','e','e','PDI UNSPSC Code','PDI OT Classification Code','Seller Item EAN']] # rearrange column here
     df_reorder.to_csv(path, header=None, index=False, sep="\t")
     
